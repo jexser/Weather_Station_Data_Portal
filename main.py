1639a834dd7240ea1377900b09755c753d9c671a
Sergei Hamzatov <drkazak@gmail.com>
2026-04-10 13:50:58 +0300

    Implemented stations list on Main

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template
 import pandas as pd
-import numpy as np
 from dataclasses import dataclass
 
 app = Flask("__name__")
 
 @dataclass 
-class CsvFields():
+class Fields():
     field_TG: str = "   TG"
     field_DATE: str = "    DATE"
+    field_STAID: str = "STAID"
+    field_STANAME: str = "STANAME                                 "
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    stations = pd.read_csv("data_small\\stations.txt", skiprows=17)
+    stations = stations[[Fields.field_STAID, Fields.field_STANAME]]
+    return render_template("home.html", data=stations.to_html())
 
 
-@app.route("/api/v1/<station>/<date>")
-def call_api(stationid: int, date) -> dict:
+@app.route("/api/v1/<stationid>/<date>")
+def call_api(stationid, date):
     file_path = "data_small\\TG_STAID" + str(stationid).zfill(6) + ".txt"
-    df = pd.read_csv(file_path, skiprows=20, parse_dates=[CsvFields.field_DATE])
-    temperature = df.loc[df[CsvFields.field_DATE] == date, [CsvFields.field_TG]].squeeze() / 10 # type: ignore
+    df = pd.read_csv(file_path, skiprows=20, parse_dates=[Fields.field_DATE])
+    temperature = df.loc[df[Fields.field_DATE] == date][Fields.field_TG].squeeze() / 10 # type: ignore
 
     return {
                 "stationid": stationid,
